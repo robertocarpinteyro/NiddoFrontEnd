f30252fd69eb5a94c659575e9926a38520f42ed7
oasiscreativa <creativa.oasis@gmail.com>
2024-09-03 13:25:55 -0600

    update

--- src/components/Desarrollos.tsx
+++ src/components/Desarrollos.tsx
@@ -207,7 +207,7 @@ export function GridDisplay() {
               <div className="flex flex-row items-center space-x-4 z-10 transition-opacity duration-300 opacity-0 group-hover/card:opacity-100">
                 <Image
                   src={desarrollo.logo.url}
-                  className=" rounded-full border-2 object-cover "
+                  className=" rounded-full border-2 object-cover w-full h-full"
                   width={50}
                   height={50}
                   alt={
