f8a7a2c4b040af3807921be8d5d71cd6937a70ff
oasiscreativa <creativa.oasis@gmail.com>
2024-09-20 00:27:29 -0600

    update

--- src/app/niddia/page.tsx
+++ src/app/niddia/page.tsx
@@ -1,11 +1,4 @@
-import { Container } from "@/components/Container";
-import { Hero } from "@/components/Hero";
-import { SectionHeading } from "@/components/SectionHeading";
-import { Benefits } from "@/components/Benefits";
-import { ContentWithImage } from "@/components/ContentWithImage";
-import { Video } from "@/components/Video";
-import { Faq } from "@/components/Faq";
-import { Cta } from "@/components/Cta";
+
 import { SidebarDemo } from "@/components/Niddia";
 import { NextUIProvider } from "@nextui-org/react";
 import { GridDisplay } from "@/components/Desarrollos";
